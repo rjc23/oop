312a7af5c1b9077ec64e41aa7953f8b8d4745a3a
Ryan J Carmody <ryancarmody@outlook.com>
2022-08-07 19:31:38 +0930

    adding input files

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void) {
+
+    int numOne;
+    int numTwo;
+
+    std::cin >> numOne;
+    std::cin >> numTwo;
+
+    std::cout << numOne + numTwo << std::endl;
+
+
+    return 0;
+}
